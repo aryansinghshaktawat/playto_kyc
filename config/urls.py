d0167cce6b1ce05554b5d6afbb192d0d481874cf
aryansinghshaktawat <shaktawat.aryan971@gmail.com>
2026-04-27 12:12:22 +0530

    refactor: clean up unused imports and static URL handling

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,5 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import path, include
 from kyc.views import home
 
 urlpatterns = [
@@ -10,6 +7,3 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("kyc.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
