338fa2d6c6a35206262856e93ca9db87c3bfa270
aryansinghshaktawat <shaktawat.aryan971@gmail.com>
2026-04-27 12:06:36 +0530

    fix routing issue

--- kyc/urls.py
+++ kyc/urls.py
@@ -1,11 +1,10 @@
-from django.urls import include, path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
 from .views import KYCSubmissionViewSet
 
 router = DefaultRouter()
-router.register("kyc", KYCSubmissionViewSet, basename="kyc")
+router.register(r'kyc', KYCSubmissionViewSet, basename='kyc')
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path('', include(router.urls)),
 ]
