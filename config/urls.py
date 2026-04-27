3203b9eb93e9c637e55cbbedfaefe749260f4a16
aryansinghshaktawat <shaktawat.aryan971@gmail.com>
2026-04-27 10:54:34 +0530

    fix deployment start command

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import KYCSubmissionViewSet
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
-router = DefaultRouter()
-router.register(r'kyc', KYCSubmissionViewSet, basename='kyc')
+from kyc.views import home
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", home, name="home"),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("kyc.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
